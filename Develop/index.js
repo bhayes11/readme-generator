a4c6fb10a70d53718b4dcdade6ec42ca19a04e62
Hayes11 <bhayes81@gmail.com>
2020-11-01 21:21:46 -0500

    added prompts to index file

--- Develop/index.js
+++ Develop/index.js
@@ -2,11 +2,80 @@
 
 const fs = require("fs");
 const inquirer = require("inquirer");
+const generate = require("./utils/generateMarkdown");
 
 
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "What badges do you want?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "What is your project description?"
+    },
+    
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the installation instructions?"
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the project usage?"
+    },
+
+    {
+        type: "input",
+        name: "license",
+        message: "What license would you like ot use?"
+    },
+
+    {
+        type: "input",
+        name: "contributing",
+        message: "What are the names of the contributors?"
+    },
+
+    {
+        type: "input",
+        name: "test",
+        message: "What are the test instructions?"
+    },
+
+    {
+        type: "input",
+        name: "badge",
+        message: "What badges do you want?"
+    },
+
+    {
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+
+
+
+
 
 ];
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,6 +2,45 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${data.badge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [License](#License)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [Questions](#Questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+${data.github}
+${data.email}
+
 `;
 }
 
